d79ce72da2ad6b93d71baf741a9aa6c9705b4e96
Chris Catlin <c.catlin@yahoo.com>
2023-12-14 21:55:29 +0000

    Dynamic password fields created

--- source/mainScript.js
+++ source/mainScript.js
@@ -4,19 +4,49 @@
 // div id's:: loginBox createBox passwordBox mainMenu
 
 
+var loggedInBool  = false;
+var pwRows        = 0;
+
+
+
+function setLogin() {
+    showElements("loginBox");
+    hideElements("createBox");
+    hideElements("passwordBox");
+    hideElements("mainMenu")
+};
+
+function setCreate() {
+    hideElements("loginBox");
+    showElements("createBox");
+    hideElements("passwordBox");
+    hideElements("mainMenu")
+};
+function setMain() {
+    hideElements("loginBox");
+    hideElements("createBox");
+    hideElements("passwordBox");
+    showElements("mainMenu")
+};
+
+function setLoggedIn() {
+    hideElements("loginBox");
+    hideElements("createBox");
+    showElements("passwordBox");
+    hideElements("mainMenu")
+};
+
 
 function hideElements(groupName) {
-    let element = document.getElementById(groupName);
+    var element = document.getElementById(groupName);
     if ( element != null ) {
-        //element.classList.add("hidden");
         element.style.display = 'none';
     }
 };
 
 function showElements(groupName) {
-    let element = document.getElementById(groupName);
+    var element = document.getElementById(groupName);
     if ( element != null ) {
-        //element.classList.remove("hidden");
         element.style.display = 'block';
     }
 };
@@ -40,31 +70,82 @@ function checkPWIn() {
 
     alert(password)
 
+    loggedInBool = true;
+
+    setLoggedIn()
+
+}
+
+function setNewPW() {
+    setLoggedIn()
+}
+
+function checkPWValid() {
+    var inputValue = getTextInput("pwInput");
+    button = document.getElementById("logInButton");
+
+    if (inputValue != "") {
+        button.disabled = false;
+    }
+    else {
+        button.disabled = true;
+    }
 }
 
 
 
-function setLogin() {
-    showElements("loginBox");
-    hideElements("createBox");
-    hideElements("passwordBox");
-    hideElements("mainMenu")
-};
+function deleteField(currentElement) {
 
-function setCreate() {
-    hideElements("loginBox");
-    showElements("createBox");
-    hideElements("passwordBox");
-    hideElements("mainMenu")
-};
+    var parentDiv = currentElement.parentElement;
+    parentDiv.parentNode.removeChild(parentDiv);
+
+}
 
 
-function setMain() {
-    hideElements("loginBox");
-    hideElements("createBox");
-    hideElements("passwordBox");
-    showElements("mainMenu") 
-};
+function newAccountBox() {
+
+    var parent    = document.getElementById("passwordBox");
+    var newAccBox = document.createElement("div");
+
+
+    var webURL = document.createElement("input");
+    webURL.type  = "text";
+    webURL.id = "websiteName" + pwRows;
+    webURL.placeholder = "Website Name";
+    newAccBox.append(webURL);
+
+    var username = document.createElement("input");
+    username.type = "text";
+    username.id = "username" + pwRows;
+    username.placeholder = "Username";
+    newAccBox.append(username);
+
+    var password = document.createElement("input");
+    password.type = "text";
+    password.id = "password" + pwRows;
+    password.placeholder = "Password";
+    newAccBox.append(password);
+
+    var deleteButton = document.createElement("button");
+    deleteButton.setAttribute("onclick", "deleteField(this)");
+    deleteButton.textContent = "Delete";
+    newAccBox.append(deleteButton);  
+
+
+    newAccBox.id = "pwFieldDiv" + pwRows; 
+
+    parent.appendChild(newAccBox);
+
+    pwRows += 1;
+
+    return newAccBox;
+}
+
+
+
+function createAccountBox() {
+    newAccountBox()
+}
 
 
 
