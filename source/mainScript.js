c58ac9969d225263e364402118e976c1e3d92e42
Chris Catlin <c.catlin@yahoo.com>
2024-04-16 16:06:12 +0100

    Final code cleanup
    DONE!!!!

--- source/mainScript.js
+++ source/mainScript.js
@@ -133,7 +133,9 @@ function decryptString(stringIn) {
 */
 
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+// checkNewPWValid - function for enabling/disabling alert elements for new acocunt creation
 function checkNewPWValid() { // Called in account creation - Create Page
 
     var newPassword = getTextInput("newPWInput");
@@ -146,34 +148,34 @@ function checkNewPWValid() { // Called in account creation - Create Page
     var containsOnlyValid = true;
     var pwMatches         = false;
     
-    for (var i = 0; i < 10; i++) {
+    for (var i = 0; i < 10; i++) {          // Check if PW contains a number 
         if (newPassword.includes(i)) {
             pwContainsNum = true;
         }
     }
 
-    for (var i = 0; i < passwordLength; i++) {
+    for (var i = 0; i < passwordLength; i++) {              // Check for not accepted chars - could cause issues with encryption otherwise
         var asciiVal = newPassword.charCodeAt(i);
         if ( (asciiVal < 48) || ((asciiVal > 57) && (asciiVal < 65)) || ((asciiVal > 90) && (asciiVal < 97)) || (asciiVal > 122) ) {
             containsOnlyValid = false;
         }
     }
 
-    if (containsOnlyValid) {
+    if (containsOnlyValid) {                  // Togggle for invalid chars alert
         hideElements("invalidChars");
     }
     else {
         showElements("invalidChars");
     }
 
-    if (pwContainsNum == false) {
+    if (pwContainsNum == false) {              // Toggle for no number alert
         showElements("noNumber");
     }
     else {
         hideElements("noNumber");
     }
-
-    if (passwordLength >= 5) {
+     
+    if (passwordLength >= 10) {              // Check PW length (!!! if <10)
         pwLongEnough = true;
         hideElements("pwTooShort");
     }
@@ -182,7 +184,7 @@ function checkNewPWValid() { // Called in account creation - Create Page
         showElements("pwTooShort");
     }
 
-    if (newPassword != confNewPassword) {
+    if (newPassword != confNewPassword) {       // Check if passwords match
         pwMatches = false;
         showElements("pwMismatch");
     }
@@ -191,9 +193,9 @@ function checkNewPWValid() { // Called in account creation - Create Page
         hideElements("pwMismatch")
     }
 
-    createButton = document.getElementById("createAccountButton");
+    createButton = document.getElementById("createAccountButton"); 
 
-    if (pwLongEnough && pwContainsNum && containsOnlyValid && pwMatches){
+    if (pwLongEnough && pwContainsNum && containsOnlyValid && pwMatches){           // Enable/Disable create button if PWs follow the rules
         createButton.disabled = false;
     }
     else{
@@ -202,8 +204,12 @@ function checkNewPWValid() { // Called in account creation - Create Page
 
 }
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+//createFileForSave - Parses contents of all PW boxes into JSON obj
 function createFileForSave()
 {
 
@@ -213,28 +219,32 @@ function createFileForSave()
     JSONObjOut.passwordHash = hashString(masterPW);
     JSONObjOut.rows = pwRows;
 
-    for (var i = 0; i < pwRows; i++)
+    for (var i = 0; i < pwRows; i++)       // For each PW Box
     {
         
-        var accountRowTemplate = JSON.parse("{ \"account\": null, \"website\": null, \"username\": null, \"password\": null}")
+        var accountRowTemplate = JSON.parse("{ \"account\": null, \"website\": null, \"username\": null, \"password\": null}")     // Create new template OBJ
 
-        var webURL   = encryptString(document.getElementById("websiteName" + i.toString()).value);
+        var webURL   = encryptString(document.getElementById("websiteName" + i.toString()).value);     // Encrypt data
         var username = encryptString(document.getElementById("username" + i.toString()).value);
         var password = encryptString(document.getElementById("password" + i.toString()).value);
 
-        accountRowTemplate.account  = i;
+        accountRowTemplate.account  = i;              // Set JSON contents
         accountRowTemplate.website  = webURL;
         accountRowTemplate.username = username;
         accountRowTemplate.password = password;
 
-        JSONObjOut.accounts.push(accountRowTemplate);
+        JSONObjOut.accounts.push(accountRowTemplate);     // Push to main JSON OBJ
 
     }
     
-    return JSON.stringify(JSONObjOut);
+    return JSON.stringify(JSONObjOut);          // Return main JSON OBJ
     
 };
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// download - called by save button to crate JSON OBJ then download to device 
 
 function download() // Prompts download of JSON file
 {  
@@ -242,85 +252,100 @@ function download() // Prompts download of JSON file
     fileName = document.getElementById("vaultNameLoggedIn").value;
     var objBlob = null;
  
-    if (objAnchorTag != null) {
-        objBlob = new Blob([createFileForSave()], { type: 'text/plain' });
+    if (objAnchorTag != null) {                       // If HREF is found
+        objBlob = new Blob([createFileForSave()], { type: 'text/plain' });       // Create new binary large obj, with contents of JSON file (above)
 
-        objAnchorTag.href = URL.createObjectURL(objBlob);
-        objAnchorTag.download = (fileName || "PassMaster.txt");
-        objAnchorTag.click();
+        objAnchorTag.href = URL.createObjectURL(objBlob); 
+        objAnchorTag.download = (fileName || "PassMaster.txt");         // Set file name
+        objAnchorTag.click();         // Simulate click on hidden HREF
     } 
 }
 
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// importFile -  reads in file when given input to file select element
 
 function importFile() { // Prompts upload of file 
 
-    var fileIn = document.getElementById("fileInput").files[0];
+    var fileIn = document.getElementById("fileInput").files[0];     // Gets file reference
 
     fileName = fileIn.name;
 
-    var fileRead = new FileReader();
-    fileRead.onload = function (e) {  
+    var fileRead = new FileReader();        // New fileReader OBJ
+    fileRead.onload = function (e) {        // ASYNC!! wait for file to load
 
         fileContents = e.target.result;
 
-        JSONObj = JSON.parse(fileContents);
+        JSONObj = JSON.parse(fileContents);         // Put contents into JSON obj
         pwRows  = JSONObj.rows;
 
-        fileLoaded = true;
+        fileLoaded = true;             // set global to true - user can only continue after this is true
 
         return true;
         }
 
-    fileRead.readAsText(fileIn);
+    fileRead.readAsText(fileIn);               // read in file, calling function above on completion
 
 return;
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// checkPWIn - checks if password hash matches the hashed input 
 function checkPWIn() {
 
-    if (document.getElementById("fileInput").value != "") {
+    if (document.getElementById("fileInput").value != "") {    // check if file is selected 
 
-        var password = getTextInput("pwInput").toString();
+        var password = getTextInput("pwInput").toString();    // stringify input text for safety
 
-        var fileHash = JSONObj.passwordHash;
+        var fileHash = JSONObj.passwordHash;     // Get hash from JSON obj
 
-        if (fileHash == hashString(password)) {
+        if (fileHash == hashString(password)) {     // compare hashes
 
-            loggedInBool   = true;
+            loggedInBool   = true;                    // Set vars
             PWmaster       = document.getElementById("masterPW");
             PWmaster.value = password;
             encKey         = password;
 
             document.getElementById("vaultNameLoggedIn").value = fileName;
 
-            setLoggedIn();
+            setLoggedIn();       // Call sub functions
             populateBoxes();    
 
             hideElements("changeText");
         }
         else {
-            alert("Incorrect credentials for selected file.");
+            alert("Incorrect credentials for selected file.");   // if hashes dont match, alert
         }
 
     }
     else {
-        alert("No file selected.")
+        alert("No file selected.")  // if file select vaule is null, alert
     }
     
 
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+//populateBoxes - function to create password boxes for JSON obj contents
 function populateBoxes() {
 
-    for (var i = 0; i < pwRows; i++) {
+    for (var i = 0; i < pwRows; i++) {      // for every row in obj...
 
-        newAccountBoxBackend(i);
-        var webURL   = document.getElementById("websiteName" + i.toString());
+        newAccountBoxBackend(i);       // create new account box
+        var webURL   = document.getElementById("websiteName" + i.toString());      // get induvidual objs
         var username = document.getElementById("username" + i.toString());
         var password = document.getElementById("password" + i.toString());
 
-        webURL.value   = decryptString(JSONObj.accounts[i].website);
+        webURL.value   = decryptString(JSONObj.accounts[i].website);     // set values
         username.value = decryptString(JSONObj.accounts[i].username);
         password.value = decryptString(JSONObj.accounts[i].password);
 
@@ -328,26 +353,40 @@ function populateBoxes() {
 
 
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// inputFieldChange - pop up reminder to save before leaving 
 function inputFieldChange() {
     showElements("changeText");
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// setNewPW - called when checkNewPW works 
 function setNewPW(password) {
 
-    setLoggedIn();
-    encKey = document.getElementById("confirmPWInput").value;
+    setLoggedIn();     // set page state
+    encKey = document.getElementById("confirmPWInput").value;        // set values of logged in page
     document.getElementById("masterPW").value = encKey;
     document.getElementById("vaultNameLoggedIn").value = document.getElementById("vaultName").value;
     
-    JSONObj = jsonTemplate; 
+    JSONObj = jsonTemplate; // creates new JSON obj for new vault
 
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-function checkPWValid() {     
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// checkPWValid - checks if log in button should be enabled 
+function checkPWValid() {      
     var inputValue = getTextInput("pwInput");
     button = document.getElementById("logInButton");
 
-    if ( (inputValue != "") && ( fileLoaded ) ) {
+    if ( (inputValue != "") && ( fileLoaded ) ) {     // if file has loaded and password field is not empty, show log in button
         button.disabled = false;
     }
     else {
@@ -356,16 +395,20 @@ function checkPWValid() {
 
 
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 
 
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// toggleShowPw - function for show/hide button
 function toggleShowPw(element) {
 
-    var elementRow = element.id.replace(/[^0-9]/g, '');
+    var elementRow = element.id.replace(/[^0-9]/g, '');      // regex for extracting the number out of the element name
 
-    var pwBox = document.getElementById("password" + elementRow);
+    var pwBox = document.getElementById("password" + elementRow);     // get input field elements
 
-    if (pwBox.type == "password") {
+    if (pwBox.type == "password") {           // toggle visibility 
         element.textContent = "Hide";
         pwBox.type = "text";
 
@@ -375,63 +418,82 @@ function toggleShowPw(element) {
         pwBox.type = "password";
     }
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// deleteField - deletes password row
 function deleteField(currentElement) {
 
-    var parentDiv = currentElement.parentElement;
+    var parentDiv = currentElement.parentElement;    // remove from parent div
     parentDiv.parentNode.removeChild(parentDiv);
-    pwRows -= 1;
+    pwRows -= 1;          // decrement password rows global
 
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 
+
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+// newAccountBox - function for dynamic pw box creation
 function newAccountBox() {
 
-    var parent    = document.getElementById("passwordBox");
+    var parent    = document.getElementById("passwordBox");    // get parent div
     var newAccBox = document.createElement("div");
 
-    var webURL = document.createElement("input");
+    var webURL = document.createElement("input");        // create URL input
     webURL.type  = "text";
     webURL.id = "websiteName" + pwRows;
     webURL.placeholder = "Website Name";
     webURL.setAttribute("onkeyup", "inputFieldChange()");
     newAccBox.append(webURL);
 
-    var username = document.createElement("input");
+    var username = document.createElement("input");   // create Username input
     username.type = "text";
     username.id = "username" + pwRows;
     username.placeholder = "Username";
     username.setAttribute("onkeyup", "inputFieldChange()");
     newAccBox.append(username);
 
-    var password = document.createElement("input");
+    var password = document.createElement("input");     // create password input
     password.type = "password";
     password.id = "password" + pwRows;
     password.placeholder = "Password";
     password.setAttribute("onkeyup", "inputFieldChange()");
     newAccBox.append(password);
 
-    var showBox = document.createElement("button");
+    var showBox = document.createElement("button");    // create show/hide button
     showBox.id = "showButton" + pwRows;
     showBox.setAttribute("onclick", "toggleShowPw(this)")
     showBox.textContent = "Show";
     newAccBox.append(showBox);
 
-    var deleteButton = document.createElement("button");
+    var deleteButton = document.createElement("button");         // create delete button
     deleteButton.setAttribute("onclick", "deleteField(this)");
     deleteButton.textContent = "Delete";
     newAccBox.append(deleteButton);  
 
 
-    newAccBox.id = "pwFieldDiv" + pwRows; 
+    newAccBox.id = "pwFieldDiv" + pwRows; // set div ID
 
-    parent.appendChild(newAccBox);
+    parent.appendChild(newAccBox);   // add div to parent
 
-    pwRows += 1;
+    pwRows += 1;  // increment password rows global
 
-    return newAccBox;
+    return newAccBox;   // return obj
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// newAccountBoxBackend - same as above but without incrementing pw rows to prevent an infinite loop
 function newAccountBoxBackend(rowNum) {
 
     var parent = document.getElementById("passwordBox");
@@ -475,39 +537,50 @@ function newAccountBoxBackend(rowNum) {
 
     return newAccBox;
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+// createAccountBox - calls creation for new account box
 function createAccountBox() {
     newAccountBox();
 }
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
+
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+//createBinaryBackdrop - function for creating random binary divs
 function createBinaryBackdrop() {
 
-    var intY              = 0;
+    var intY              = 0;     // set vars
     var intDivHeight      = 20;
     var objDiv            = null;
-    var objParentDiv = document.getElementById( 'binaryBackdrop' );
+    var objParentDiv = document.getElementById( 'binaryBackdrop' );   // get parent
 
     if ( objParentDiv != null ) {
-        for ( intY = 0; intY < (document.documentElement.clientHeight ); intY += intDivHeight ) {
+        for ( intY = 0; intY < (document.documentElement.clientHeight ); intY += intDivHeight ) {       // get height of screen, repeat until screen is full without overflow
             objDiv = document.createElement( 'div' );
             if ( objDiv != null ) {
-                objDiv.id             = 'binaryDiv_' + binaryDivCount.toString().padStart( 4, '0' );
+                objDiv.id             = 'binaryDiv_' + binaryDivCount.toString().padStart( 4, '0' );        // create new div and add css
                 objDiv.style.position = 'absolute';
                 objDiv.style.top      = intY.toString() + 'px';
                 objDiv.style.left     = '0px';
                 objDiv.style.maxWidth = objParentDiv.offsetWidth;
-                objDiv.style.height   = intDivHeight.toString() + 'px';
+                objDiv.style.height   = intDivHeight.toString() + 'px';        
                 objDiv.innerHTML = '';
                 objParentDiv.appendChild(objDiv);
 
                 while (objDiv.offsetWidth < objParentDiv.offsetWidth) {
-                   objDiv.innerHTML += '&nbsp;' + Math.floor(Math.random() * 2).toString() + '&nbsp;';
+                   objDiv.innerHTML += '&nbsp;' + Math.floor(Math.random() * 2).toString() + '&nbsp;';    // fill div with random 1s and 0s
                 }
-                binaryDivCount++;
-                } //if-div-created
-            } //for-y
-        } //if
-
-return;
+                binaryDivCount++; // increment counter
+                } 
+            } 
+        } 
 }    
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+
